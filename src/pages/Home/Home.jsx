2c1e9ed615ed982099cdb6587b07efe7f0d4c399
Kalash Bhadoriya <kalashbhadoriya7147@gmail.com>
2024-12-20 10:42:24 +0530

    Initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Menu from '../Menu/Menu'
+import Hero from '../../components/Hero'
+
+const Home = () => {
+ 
+
+  return (
+    <>
+      <Hero/>
+      <Menu/>
+    </>
+  )
+}
+
+export default Home
+
+
